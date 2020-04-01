4fba3a56b1e3f331c75d5ed9d7927a9e12178967
Noureddine LOUAHEDJ <n.louahedj@gmail.com>
2020-04-01 18:33:12 +0100

    Added some changes

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,34 @@
 from django.contrib import admin
-from .models import Category, Product, Brand
+from admirarchy.toolbox import HierarchicalModelAdmin, AdjacencyList
+from .models import Category, Product, Brand, Tag
 
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('abbreviation', 'name')
+    list_filter = ('is_active', )
+    search_fields = ('abbreviation', 'name')
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('abbreviation', 'name', 'parent')
+class CategoryAdmin(HierarchicalModelAdmin):
+    list_display = ('abbreviation', 'name', 'parent', 'is_active')
+    list_filter = ('is_active', )
+    autocomplete_fields = ('parent', )
+    search_fields = ('abbreviation', 'name', 'parent__name', 'parent__abbreviation')
+    hierarchy = True
+
+
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('reference', 'bar_code', 'name', 'brand', 'category')
+    search_fields = ('reference', 'bar_code', 'name', 'full_name', 'description')
+    autocomplete_fields = ('brand', 'tags', 'category')
 
 
 # Register your models here.
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Brand(models.Model):
     abbreviation = models.CharField(max_length=10, unique=True, blank=False)
     name = models.CharField(max_length=150, blank=False)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -13,7 +14,19 @@ class Category(models.Model):
     abbreviation = models.CharField(max_length=10, unique=True, blank=False)
     name = models.CharField(max_length=150, blank=False)
     parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
-    depth = models.IntegerField(default=0, blank=False)
+    #depth = models.IntegerField(default=0, blank=False)
+    is_active = models.BooleanField(default=True)
+
+    @property
+    def depth(self):
+        """Get depth"""
+        p = self.parent
+        if p:
+            return 1 + p.depth
+        else:
+            return 0
+
+    #depth = property(_depth)
 
     def __str__(self):
         return self.name
@@ -22,13 +35,18 @@ class Category(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True, null=False)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     reference = models.CharField(max_length=30, blank=False, unique=True)
     bar_code = models.CharField(max_length=100, blank=False, unique=True)
     name = models.CharField(max_length=100, blank=False)
-    full_name = models.TextField()
-    description = models.TextField()
-    active = models.BooleanField(default=True)
+    full_name = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    # additional information
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, null=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, null=False)
+    tags = models.ManyToManyField(Tag)
